7494db0a09858b8c9bdad983c550262bd8553001
Osama <osama.bmaq@gmail.com>
2021-11-16 17:21:15 +0200

    test

---
+++ src/test/java/test/TestClass.java
@@ -0,0 +1,14 @@
+package test;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestClass {
+    @Test
+    void success() {
+        assertTrue(true);
+    }
+}
